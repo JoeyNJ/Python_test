c5af396257e7a11f8d424e9b8ffc262536e743e7
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-07-21 22:40:51 +0800

    Expanded on if statements

--- if_statements.py
+++ if_statements.py
@@ -98,3 +98,82 @@ if user not in banned_users:
 # Boolean Expressions
 game_active = True
 can_edit = False
+
+
+#Simple if Statements
+#has one test and one action:
+
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")   # you can add as many lines of code indented to be executed
+    # if the value of age is less than 18, this will produce nothing
+    
+#if-else Statements
+# similar to if statemens,k but else statement define an action to execute when the condition fails
+
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+    
+#if-elif-else Chain
+# to test more than 2 situations, only executes 1 block in an if-elif-else chain:
+
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
+    
+# rather than printing the admission price within the block, its more concise to print after the chain evaluated:
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+    
+print(f"Your admission cost is ${price}.")
+
+#Using multiple elif Blocks:
+# you can use as many elif blocks in you code:
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+elif age > 65:   # else block can be ommited, but the blocks must atleast pass one test to execute the print call:
+    price = 20
+    
+print(f"Your admission cost is ${price}.")
+
+#Testing Multiple Conditions
+# use a series of simple if statements w/ no elif or else blocks:
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+    
+print("\nFinished making your pizza!")
+
+#Summary: if you want only one block of code to run, use an if-elif-else chain.
+#If more than one block of code needs to run, use a series of independent if statements
