f1ad6307c51b0c331ecac5ad92bb572790a450b6
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-07-22 20:35:08 +0800

    Expanded on if statements with lists

--- if_statements.py
+++ if_statements.py
@@ -177,3 +177,48 @@ print("\nFinished making your pizza!")
 
 #Summary: if you want only one block of code to run, use an if-elif-else chain.
 #If more than one block of code needs to run, use a series of independent if statements
+
+
+#Checking for special items
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}.")
+
+print("Finished making your pizza!")
+
+#What if we run out of green peppers?
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+        
+print("\nFinished making your pizza!")
+
+
+#What if the list is empty;
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+    
+    
+#Using multiple list;
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}")
+    else:
+        print(f"Sorry, we don't have {requested_topping}")
+        
+print("\nFinished making your pizzza!")
