db5b965a4f0315c8e0af4708564920873707be88
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-07-24 21:51:48 +0800

    Finished 6-1 Exercise

---
+++ practice/6-1_person.py
@@ -0,0 +1,7 @@
+# use a dictionary to store info about a person you know
+
+joan_info = {'first_name': 'joan', 'last_name': 'nicolas', 'age': 22, 'city': 'bulacan'}
+print(joan_info['first_name'].title())
+print(joan_info['last_name'].title())
+print(joan_info['age'])
+print(joan_info['city'].title())
