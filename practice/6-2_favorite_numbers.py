244eb6a5d64628594dfad9db801d5c384ce395df
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-07-25 19:39:07 +0800

    Finished exercises 6-2 and 6-3

---
+++ practice/6-2_favorite_numbers.py
@@ -0,0 +1,9 @@
+# use dictionary to print people's favorite number
+
+favorite_num = {'joey': 24, 'joan': 2, 'nenita': 28, 'boy': 10, 'carding':2}
+
+print(f"Joey's favorite number is {favorite_num['joey']}")
+print(f"Joan's favorite number is {favorite_num['joan']}")
+print(f"Nenita's favorite number is {favorite_num['nenita']}")
+print(f"Boy's favorite number is {favorite_num['boy']}")
+print(f"Carding's favorite number is {favorite_num['carding']}")
---
+++ practice/6-3_glossary.py
@@ -0,0 +1,15 @@
+# think of 5 programming words and store their meaning as values
+
+programming_words = {
+    'list': 'Is a collection of items in a particular order.',
+    'tuple': 'Is a kind of list where you cannot change its values.',
+    'if statements': 'Is where you check a condition and see if it is true or false to run a code.',
+    'print()': 'Is a function in Python where you can display/print inside the parenthesis.',
+    'dictionaries': 'Is a collection of key=value pairs.',
+}
+
+print(f"List:\n{programming_words['list']}")
+print(f"\nTuple:\n{programming_words['tuple']}")
+print(f"\nIf-Statements:\n{programming_words['if statements']}")
+print(f"\nprint():\n{programming_words['print()']}")
+print(f"\nDictionaries:\n{programming_words['dictionaries']}")
