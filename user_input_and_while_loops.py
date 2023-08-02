4dd7a342a6b692271692eaa98b378400fc88f7d3
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-08-02 19:27:13 +0800

    Progressed on the lesson

--- user_input_and_while_loops.py
+++ user_input_and_while_loops.py
@@ -36,10 +36,49 @@
 # The Modulo Operator
 # even or odd
 
-number = input("Enter a number, and I'll tell you if it's even or odd: ")
-number = int(number)
+#number = input("Enter a number, and I'll tell you if it's even or odd: ")
+#number = int(number)
 
-if number % 2 == 0:
-    print(f"\nThe number {number} is even.")
-else:
-    print(f"\nThe number {number} is odd.")
+#if number % 2 == 0:
+#    print(f"\nThe number {number} is even.")
+#else:
+#    print(f"\nThe number {number} is odd.")
+    
+    
+# Using while loop with Lists and Dictionaries
+# moving items from One List to another
+
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
+    
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+    
+    
+# Removing All Instances of Specific Values from a List
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+    
+print(pets)
+
+
+# Filling a Dictionary with User Input
+
+responses = {}
+
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday")
