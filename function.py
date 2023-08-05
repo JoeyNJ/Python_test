9bc7be0b154c685a3cdda5b9e5c52b21d7064b48
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-08-05 19:47:41 +0800

    Progressed on the lesson about Functions!

--- function.py
+++ function.py
@@ -6,4 +6,39 @@ def greet_user():
 
 greet_user()
 
-num = input("POUT NUM ")
+# Passing info to a Function
+
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+greet_user('jesse')
+
+# Positional Arguments
+
+def describe_pet(animal_type, pet_name):
+    """Display info about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')
+
+# Multiple Function Calls
+
+describe_pet('dog', 'willie')
+describe_pet('cat', 'tiger')
+
+# Order matters in positional arguments
+
+describe_pet('harry', 'hamster')
+
+# Keyword Arguments
+
+def describe_pet(animal_type, pet_name):
+    """Displays information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet(animal_type='hamster', pet_name='harry') # this is much better
+describe_pet(pet_name='harry', animal_type='hamster') # oder doesn't matter
+    
