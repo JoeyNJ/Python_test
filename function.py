0b82515135bf217b6ec351bd1d3aa47fbf98f406
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-08-06 19:42:32 +0800

    Progressed on the lesson about Function

--- function.py
+++ function.py
@@ -41,4 +41,34 @@ def describe_pet(animal_type, pet_name):
     
 describe_pet(animal_type='hamster', pet_name='harry') # this is much better
 describe_pet(pet_name='harry', animal_type='hamster') # oder doesn't matter
+
+# Default values
+# you can set a default value if an argument is not available
+
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
     
+describe_pet(pet_name='willie') # python still considers this a positional arguments
+describe_pet(pet_name='harry', animal_type='hamster')
+
+# Equivalent Function Calls
+
+# a dog named Willie
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+# a hamster named Harry
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry',animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+# Avoiding Argument Errors
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+    
+describe_pet()
