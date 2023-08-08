df4874a8cdfe7597cffc21da86a802a11a1c7d9f
JoeyLP <11444333+JoeyNJ@users.noreply.github.com>
2023-08-08 20:10:29 +0800

    Progressed on lesons about functions

--- function.py
+++ function.py
@@ -133,3 +133,26 @@ def build_person(first_name, lastname, age=None):
 
 musician = build_person('jimi', 'hendrix', age=56)
 print(musician)
+
+
+# Using a Function with a while loop
+
+def get_formatted_name(first_name, last_name):
+    """Return a fll name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+# This is an infinite loop
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' anytime to quit.)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+    
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}")
